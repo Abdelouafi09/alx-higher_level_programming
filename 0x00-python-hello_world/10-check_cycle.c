8c878ff08af80eb80fca966af264fa98c77d428d
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-10-31 02:24:43 +0000

    100

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * check_cycle - check if a single linked list has a cercle in it
+ * @list: pointer to the first node of the linked list
+ * Return: 0 if there is no cercle, 1 if there is
+ */
+int check_cycle(listint_t *list)
+{
+
+}
