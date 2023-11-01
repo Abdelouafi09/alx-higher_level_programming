792489ebd8b429b4c81169150f3404979c70e62b
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-11-01 02:12:50 +0000

    task 6 v2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(9):
-    for y in range(i+1,10):
-        print("{}{}, ".format(i, y), end="")
-    if range(i+2, 10) == range(10, 10):
-       print("{}{}".format(i, y))
+for i in range(10):
+    for y in range(i+1, 10):
+        if i == 8 and y == 9:
+            print("{}{}".format(i, y))
+        else:
+            print("{}{}, ".format(i, y), end="")
