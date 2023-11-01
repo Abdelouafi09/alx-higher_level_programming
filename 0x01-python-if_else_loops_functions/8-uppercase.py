ea4b4b75fa02b729e0f3c24cf136e54d131afbcf
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-11-01 02:52:53 +0000

    task 8 v2: to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) < 123 and ord(str[i]) > 96:
-            str[i] = chr(ord(str[i] - 32))
-    print(str)
+    for i in str:
+        if ord(i) < 123 and ord(i) > 96:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
