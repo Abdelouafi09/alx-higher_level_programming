b50bc643073d73e08152743a9d869a2b89afae4f
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-11-02 23:57:10 +0000

    task 0 v1: add two int

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-from add_0 import add
-a, b = 1, 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if --name-- == "__main__":
+    from add_0 import add
+    a, b = 1, 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
