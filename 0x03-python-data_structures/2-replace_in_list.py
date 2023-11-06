3ed4835149495f39dcac2fabaceeccfbf6969d94
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-11-06 23:18:14 +0000

    replac an element at idx position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if (idx >= 0) | (idx < len(my_list)):
+    if (idx >= 0) and (idx < len(my_list)):
         my_list[idx] = element
     return my_list
+
+my_list = [1, 2, 3, 4, 5]
+print("old {}".format(my_list))
+idx = 3
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+
+print("original {}".format(my_list))
+print("new {}".format(new_list))
+replace_in_list(my_list, 0, 0)
+print("0 {}".format(my_list))
+replace_in_list(my_list, -1, -1)
+print("-1 {}".format(my_list))
+replace_in_list(my_list, 5, 100)
+print("5 {}".format(my_list))
