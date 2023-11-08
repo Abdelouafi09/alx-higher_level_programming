8222fbd6273659ec35f4ae7a10f047f4b95ed669
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-11-08 02:21:06 +0000

    string wih no Cc v3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ''
     for character in my_string:
-        if char not in [C, c]:
+        if character != 'C' and character != 'c':
             new_string += character
     return new_string
