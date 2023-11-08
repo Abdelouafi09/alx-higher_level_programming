9e0c2e49d05eca870be10328eae6438e3a45a230
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-11-08 02:04:21 +0000

    replace in list copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = []
     if my_list is None:
         return
-
-    for i in range(len(my_list)):
-        new_list[i] = my_list[i]
-    if (idx >= 0) | (idx < len(my_list)):
+    new_list = my_list[:]
+    if idx >= 0 and idx < len(my_list):
         new_list[idx] = element
     return new_list
