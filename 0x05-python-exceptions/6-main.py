1259e36a5d12593c63b4ec31aaad4fa786a8fb73
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2024-02-05 00:18:52 +0000

    task 6 v1: raise exception message

--- 0x05-python-exceptions/6-main.py
+++ 0x05-python-exceptions/6-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+
 try:
-    raise_exception_msg("C is fun")
+    message = "C is fun"
+    raise_exception_msg(message)
 except NameError as ne:
     print(ne)
